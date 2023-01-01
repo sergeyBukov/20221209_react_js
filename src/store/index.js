6b9e2fc5004aa59459fb51a100733b909d4e1d31
sergey.Bukov <sergey.Bukov@yandex.ru>
2023-01-01 14:19:45 +0300

    lesson_6

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
 import { createStore, compose, combineReducers } from 'redux'
 import { profileReducer } from './profile/reducer'
-
-
+import { messagesReducer } from './messages/reducer'
 /* для отображения в браузере REDUX наши Store в который мы передаем наши Reducer*/
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
 const rootReducer = combineReducers({
     profile: profileReducer,
+    messages: messagesReducer
 })
 
 /* export const store = createStore(profileReducer, composeEnhancers()) */
